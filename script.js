3ce7bb2b7219e676c6e5fa0dde695ca7c7c21286
Kgabo71 <73986122+Kgabo71@users.noreply.github.com>
2024-06-28 09:51:29 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,74 @@
+
+const search_form = document.querySelector(".header_form");
+
+search_form.addEventListener("submit", (event) => {
+    
+    event.preventDefault();
+
+
+    const value = document.querySelector("#search").value;
+
+    search_Ip_Address(value);
+});
+
+
+async function search_Ip_Address(ip_address) {
+    const api_key = "YOUR_API_KEY";
+    try {
+        const request = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${api_key}&ipAddress=${ip_address}`);
+        const response = await request.json();
+
+        const { location, ip, isp } = response;
+
+      
+        update_ui(ip, location.city, location.timezone, isp);
+
+        
+        if (map !== undefined && map !== null) {
+            map.remove();
+        }
+        create_map(location.lat, location.lng, location.country, location.region);
+    } catch (error) {
+        console.error("Error fetching IP address data:", error);
+    }
+}
+
+
+let map;
+function create_map(lat, lng, country, region) {
+    map = L.map('map').setView([lat, lng], 14);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 20,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+ 
+    const my_icon = L.icon({
+        iconUrl: '/images/icon-location.svg',
+        iconSize: [40, 60],
+        iconAnchor: [22, 94],
+        popupAnchor: [-3, -76],
+    });
+    L.marker([lat, lng], { icon: my_icon }).addTo(map)
+        .bindPopup(`${region}, ${country}`)
+        .openPopup();
+}
+
+
+function update_ui(ip_address, location, timezone, isp) {
+   
+    const address = document.querySelector(".address");
+    const city = document.querySelector(".location");
+    const utc = document.querySelector(".utc");
+    const isprovider = document.querySelector(".isp");
+
+    
+    address.textContent = ip_address;
+    city.textContent = location;
+    utc.textContent = 'UTC' + timezone;
+    isprovider.textContent = isp;
+}
+
+
+const defaultIp = "29.44.89.169";
+search_Ip_Address(defaultIp);
